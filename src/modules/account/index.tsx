d99f7089ffc7fb8365bc675d28217ae68dff4835
nguyenconghieudata <hieunc@farmcode.io.vn>
2025-02-05 08:51:41 +0700

    fix:bug

--- src/modules/account/index.tsx
+++ src/modules/account/index.tsx
@@ -1,43 +1,36 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-import AccountProfile from './components/profile';
-import OrderHistory from './components/history';
-import OrderSingleCreate from './components/order-single';
-import Loading from './components/loading';
-import OrderAlbumCreate from './components/order-album';
+import React, { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import AccountProfile from "./components/profile";
+import OrderHistory from "./components/history";
+import OrderSingleCreate from "./components/order-single";
+import Loading from "./components/loading";
+import OrderAlbumCreate from "./components/order-album";
 
 export default function AccountClient() {
-
   const param = useSearchParams();
 
-  const [tab, setTab] = React.useState('');
+  const [tab, setTab] = React.useState("");
 
   const renderTab = () => {
     switch (tab) {
-      case 'profile':
+      case "profile":
         return <AccountProfile />;
-      case 'history':
+      case "history":
         return <OrderHistory />;
-      case 'order-single':
-        return <OrderSingleCreate />;
-      case 'order-album':
+      // case "order-single":
+      //   return <OrderSingleCreate />;
+      case "order-album":
         return <OrderAlbumCreate />;
       default:
         return <Loading />;
     }
-  }
+  };
 
   useEffect(() => {
-    setTab(param.get('tab') || 'profile');
+    setTab(param.get("tab") || "profile");
   }, [param]);
 
-  return (
-    <div className="w-full">
-      {
-        renderTab()
-      }
-    </div>
-  );
+  return <div className="w-full">{renderTab()}</div>;
 }
